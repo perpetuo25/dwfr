6e50cb99c056d9e07103ca519ceb79d9cb670ae9
Eric Sanchez <lince97@ciencias.unam.mx>
2022-01-05 17:58:17 -0600

    showing random productos in home, and detail product page

--- src/app/modules/product/component/product/product.component.ts
+++ src/app/modules/product/component/product/product.component.ts
@@ -4,6 +4,9 @@ import { Category } from '../../_model/category';
 import { ProductService } from '../../_service/product.service';
 import { CategoryService } from '../../_service/category.service';
 import { FormBuilder, Validators} from '@angular/forms';
+import { ProductImage } from '../../_model/productImage';
+import { Cart } from '../../../../shared/cart'
+import { ProductImageService } from '../../_service/product-image.service';
 import { Router } from '@angular/router';
 
 declare var $: any;
@@ -15,13 +18,26 @@ import Swal from 'sweetalert2';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
-
+export class ProductComponent implements OnInit {  
+  cart: Product[] = [];
   // Datos del producto
   products: Product[] = [];
+  product1: Product = new Product();
+  productsCat: Product[] = [];
+  products2: Product[] = [];
+  gtin: any = null;
 
   // Categorías asociadas al producto
   categories: Category[] = [];
+  category: Category = new Category();
+
+  // Imagenes del producto
+  images: ProductImage[] = [];
+  image: ProductImage = new ProductImage();
+  imagesFirst: ProductImage[] = [];
+  file: any;
+  imageChangedEvent: any;
+  base64: any;
 
   // Formulario para registrar los datos el producto
   formulario = this.formBuilder.group({
@@ -40,6 +56,7 @@ export class ProductComponent implements OnInit {
 
   constructor(
     private product_service: ProductService,
+    private product_image_service: ProductImageService,
     private category_service: CategoryService,
     private formBuilder: FormBuilder,
     private router: Router
@@ -47,18 +64,87 @@ export class ProductComponent implements OnInit {
 
   ngOnInit(): void {
     this.getProducts();
+
   }
 
-  // Read, Create y Delete de Producto --------------------------------------------------
+  addToCart(product: Product){
+    Cart.cart.push	(product);
+    console.log(Cart.cart);
+  }
 
-  getProducts(){
-    this.product_service.getProducts().subscribe(
+  get1Product(id: number){
+    for(let prod of this.products2){
+      if(prod.id_product == id){
+        this.addToCart(prod);
+      }
+    }
+  }
+  // Read, Create y Delete de Producto --------------------------------------------------
+  getProduct(gtin: string){
+    this.product_service.getProduct(gtin).subscribe(
       res => {
-        this.products = res;
+        this.product1 = res;        
+        this.getCategory(this.product1.id_category); 
+        this.getProductImages(this.product1.id_product);       
+        let temp = res;
+        this.products2.push(temp);        
       },
       err => console.log(err)
     )
   }
+  getProducts(){
+    this.product_service.getProducts().subscribe(
+      res => {
+        this.products = res;        
+        for(let p of this.products){
+          this.getProduct(p.gtin);                      
+        }
+        this.products2 = this.shuffleProducts(this.products2);                                       
+      },
+      err => console.log(err)
+    )
+    
+  }
+
+  shuffleProducts(array: Product[]){
+    let counter = array.length;
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;       
+  }
+
+  getProductImages(id_product: number){
+    this.product_image_service.getProductImages(id_product).subscribe(
+      res => {
+        this.images = res;
+        this.imagesFirst.push(this.images[0])  
+        console.log(this.imagesFirst)      
+      },
+      err => console.log(err)
+    )
+  }
+
+  getCategory(id_category: number){
+    this.category_service.getCategory(id_category).subscribe(
+      res => {
+        this.category = res;
+      },
+      err => console.log(err)
+    )
+  }
+
+
 
   onSubmit(){
     this.submitted = true;
