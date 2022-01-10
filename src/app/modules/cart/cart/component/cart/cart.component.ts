ad59395116e74ddafe99c05e87bc748b1e86b2d7
Eric Sanchez <lince97@ciencias.unam.mx>
2022-01-09 23:39:57 -0600

    categories cart total and subtotal added

--- src/app/modules/cart/cart/component/cart/cart.component.ts
+++ src/app/modules/cart/cart/component/cart/cart.component.ts
@@ -7,16 +7,42 @@ import { Cart } from '../../../../../shared/cart'
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+
 export class CartComponent implements OnInit {
-  
+  total: number = 0;
+  totalWithTaxes : number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getTotalPrice();
   }
 
   getCart(){
     let cart1: Product[] = [];
     cart1 = Cart.cart;
     return cart1;
+  }
+
+  cartCount(){
+    return Cart.cart.length;
+  }
+
+  getTotalPrice(){
+    let total: number = 0;
+    for(let prod of Cart.cart){
+      total = total + prod.price;
+    }
+    this.total = total;   
+    console.log(this.total);
+    console.log(this.totalWithTaxes);
+    return total;
+  }
+
+  getTaxes(){
+    let taxes = this.getTotalPrice();
+    taxes = taxes + (taxes * 0.16);
+    this.totalWithTaxes = taxes;
+    return taxes;
   }
 }
